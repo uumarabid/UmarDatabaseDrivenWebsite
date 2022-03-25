9eb661975558c693ae461cd6cb2c94191eaba821
Umar1311829 <star_umar2008@hotmail.com>
2022-03-25 22:05:52 +0000

    rename database

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=toy', 
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=toystore', 
    'uumar', 'zap');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
