afa6a8bd81f01a6a634d4dfe7e19916feb97a7b1
Umar1311829 <star_umar2008@hotmail.com>
2022-03-26 21:14:07 +0000

    add table

--- welcome.php
+++ welcome.php
@@ -1,15 +1,34 @@
 <?php
+require_once "pdo.php";
 session_start();
 
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
     exit();
 }
+
 ?>
 
 <html>
     <head>
         <title>Home Page</title>
     </head>
-    <body><h1>Welcome  <?= $_SESSION['username']; ?></h1></body>
+    <body><h1>Welcome  <?= $_SESSION['username']; ?></h1>
+    
+        <?php 
+        echo '<table border="1">'."\n";
+foreach ( $rows as $row ) {
+    echo "<tr><td>";
+    echo($row['id']);
+    echo("</td><td>");
+    echo($row['name']);
+    echo("</td><td>");
+    echo($row['picture']);
+    echo("</td></tr>\n");
+}
+echo "</table>\n";
+        
+        ?>
+        
+    </body>
 </html>
