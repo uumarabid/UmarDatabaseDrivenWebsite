ef0ebc5029bb417d91c2b363c6d7170af7ecc763
Umar1311829 <star_umar2008@hotmail.com>
2022-05-01 15:08:45 +0100

    create footer page

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Footer</title>
+    <link rel="icon" href="./images/super-sport.jpg" type="image/icontype" />
+    <link href="css/supersportstyle.css" type="text/css" rel="stylesheet" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+
+  <body>
+    <section>
+      <footer>
+        <h3>Our Address:</h3>
+        <address>Ashton Old Road, Openshaw, Manchester, M11 2WH</address>
+        <a href="mailto:supersport@info.co.uk">Email Super Sport</a>
+        <h3>Copyrights &copy; refers to the legal rights of the owner.</h3>
+      </footer>
+    </section>
+  </body>
+</html>
+
