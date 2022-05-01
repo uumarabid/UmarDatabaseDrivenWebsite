ebcf07fc34aa8e1643d8ff0e54ccb9239178cbfc
Umar1311829 <star_umar2008@hotmail.com>
2022-05-01 17:09:38 +0100

    connecting with hosting data base

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=toystore', 
-   'uumar', 'zap');
+$pdo = new PDO('mysql:host=sql209.epizy.com;port=21;dbname=epiz_31615791_umar', 
+   'epiz_31615791', 'tBtYewQruqlYs');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //$stmt = $pdo->query("SELECT * FROM users");
